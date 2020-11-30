6d1eb0bcf7ee2506154855b2aa3dd3077aa88ee5
Skyeward <mattyarnold@hotmail.co.uk>
2020-11-30 01:45:07 +0000

    Mat's template ideas

---
+++ name_gen.py
@@ -0,0 +1,139 @@
+#WORD TEMPLATES:
+#_____
+#_____all
+#_____bank
+#Black_____
+#_____bury
+#_____caster?
+#_____chester?
+#_____church (exclude "_____ Church" template?)
+#_____combe
+#_____court (exclude "_____ Court" template?)
+#_____dale
+#_____don
+#_____er
+#_____ey
+#_____ford
+#_____ham
+#_____ing
+#_____ley
+#Middle_____
+#_____minster
+#_____ney
+#_____pool
+#_____row
+#_____sey
+#_____side
+#_____smith
+#_____stead
+#_____stone
+#_____stow
+#_____ton
+#Up_____
+#_____water
+#____way (exclude "_____ Way" template?)
+#_____well
+#_____wich
+#_____wick
+#_____worth
+
+
+#PHRASE TEMPLATES:
+#_____
+#_____ Airport?
+#_____ Bridge
+#_____ Canal
+#_____ Central
+#_____ Church
+#_____ Court
+#_____ Cross
+#_____ Green
+#_____ Heath
+#High _____
+#Higher _____
+#_____ Hill
+#_____ Junction
+#_____ Lane
+#Little _____
+#Lower _____
+#_____ Main
+#Old _____
+#_____ Palace
+#_____ Park
+#_____ Quays
+#_____ Rise
+#_____ Road
+#Royal _____
+#Saint _____
+#_____ Square
+#_____ Street
+#_____ Town
+#_____ Walk
+#_____ Way
+#_____ Weir
+#_____ Wharf
+
+#_____ East (should these be location specific?)
+#_____ North (these can go first or second)
+#_____ South
+#_____ West (should they make a different direction version likely?)
+
+#Three [english word]s
+#Four [english word]s
+#Five [english word]s
+#Six [english word]s
+#Seven [english word]s
+#Eight [english word]s
+
+
+#ENGLISH SOUNDING WORDS
+#Arch
+#Avenue
+#Barrow
+#Bishop
+#Bluffs
+#Borough
+#Broadway
+#Brook
+#Chapel
+#Corner
+#Cottage
+#Court
+#Creek
+#Deacon
+#Dock(s)
+#Farm
+#Field(s)
+#Fountain
+#Garden
+#Gate
+#Grange
+#Grove
+#Hamlet
+#Harbour
+#High Street
+#House
+#Manor
+#Meadows
+#Mill
+#King('s)
+#Oak
+#Oval
+#Path
+#Queen('s)
+#Stables
+#Tree
+#Tunnel
+#Vale
+#Village
+#Water (exclude "_____water" template?)
+#Weir
+#Wood(s)
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
