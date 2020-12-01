6d1ff9278150dd6d17ccc6f66af8f03c26fa08fb
Skyeward <mattyarnold@hotmail.co.uk>
2020-12-01 20:03:01 +0000

    word bank json started, formatting created

--- name_gen.py
+++ name_gen.py
@@ -3,7 +3,9 @@
 #_____all
 #_____bank
 #Black_____
+#_____bottom
 #_____bury
+#_____by
 #_____caster?
 #_____chester?
 #_____church (exclude "_____ Church" template?)
@@ -11,31 +13,41 @@
 #_____court (exclude "_____ Court" template?)
 #_____dale
 #_____don
+#_____end (exclude "_____ End" template?)
 #_____er
 #_____ey
 #_____ford
+#_____hall
 #_____ham
 #_____ing
 #_____ley
 #Middle_____
 #_____minster
+#_____mouth
 #_____ney
 #_____pool
 #_____row
+#_____sea
+#_____(s)end
 #_____sey
 #_____side
 #_____smith
+#_____stall
 #_____stead
 #_____stone
 #_____stow
 #_____ton
 #Up_____
+#_____wake
+#_____wall
+#_____wark
 #_____water
 #____way (exclude "_____ Way" template?)
 #_____well
 #_____wich
 #_____wick
 #_____worth
+#_____wright
 
 
 #PHRASE TEMPLATES:
@@ -43,20 +55,28 @@
 #_____ Airport?
 #_____ Bridge
 #_____ Canal
+#_____ Carriageway
 #_____ Central
 #_____ Church
+#City _____
 #_____ Court
 #_____ Cross
+#_____ End
+#_____ Exchange
+#_____ Garden(s)
+#Greater _____
 #_____ Green
 #_____ Heath
 #High _____
 #Higher _____
 #_____ Hill
+#Inner _____
 #_____ Junction
 #_____ Lane
 #Little _____
 #Lower _____
 #_____ Main
+#_____ Market
 #Old _____
 #_____ Palace
 #_____ Park
@@ -67,11 +87,14 @@
 #Saint _____
 #_____ Square
 #_____ Street
+#_____ Tower
 #_____ Town
+#_____ Vale
 #_____ Walk
 #_____ Way
 #_____ Weir
 #_____ Wharf
+#_____ Wood(s)
 
 #_____ East (should these be location specific?)
 #_____ North (these can go first or second)
@@ -86,49 +109,61 @@
 #Eight [english word]s
 
 
-#ENGLISH SOUNDING WORDS
+#WORD BANK
 #Arch
 #Avenue
 #Barrow
+#Bell
 #Bishop
 #Bluffs
 #Borough
 #Broadway
 #Brook
+#Castle
 #Chapel
+#Cobbles
 #Corner
 #Cottage
 #Court
 #Creek
 #Deacon
 #Dock(s)
+#Duke 
+#Estate
 #Farm
 #Field(s)
 #Fountain
-#Garden
+#Gallery
 #Gate
+#Glade
 #Grange
 #Grove
 #Hamlet
 #Harbour
 #High Street
 #House
+#King('s)
 #Manor
 #Meadows
+#Mere
+#Mile
 #Mill
-#King('s)
+#Monger?
 #Oak
 #Oval
 #Path
+#Pond
 #Queen('s)
+#Rectory
+#Runway
 #Stables
+#Stadium
 #Tree
 #Tunnel
-#Vale
 #Village
 #Water (exclude "_____water" template?)
 #Weir
-#Wood(s)
+#Yard
 
 
 def main():
